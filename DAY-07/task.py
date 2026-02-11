1fc7de34b09332dc224f636ca08709122da6c71d
Aarin Diwan <adiwan2007@gmail.com>
2026-02-11 06:19:10 +0000

    cc

---
+++ DAY-07/task.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+ed = pd.read_csv("Early_Learning_Feedback_Report_-_Number_of_Domains_Ready.csv")
+
+#print(ed.head(10))
+print(ed.tail(15))
+#print(ed.info(10))
+#print(ed.describe(10))
