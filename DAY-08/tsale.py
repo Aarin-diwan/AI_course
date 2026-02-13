faa91d2c51c9c7b24bd12bb0f85e89c72f1ac84d
Aarin Diwan <adiwan2007@gmail.com>
2026-02-13 03:36:09 +0000

    cc

--- DAY-08/tsale.py
+++ DAY-08/tsale.py
@@ -2,6 +2,6 @@ import pandas as pd
 d = pd.read_csv("coffee_shop_sales.csv")
 d["total_price"] = d["transaction_qty"] * d["unit_price"]
 tsales = 0
-tsales += d["total_price"]
-print(tsales)
+tsales += d["total_price"].sum()
+print("Total sales: ",tsales)
 print(d.head())
